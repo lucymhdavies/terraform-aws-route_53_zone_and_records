72dfba417bfa9a5596a8c7869648d757a30c20f4
Lucy Davinhart <lucy@lucymhdavies.com>
2022-04-19 18:53:34 +0100

    More outputs, including zone arn and ID
    Workaround for https://github.com/lucymhdavies/terraform-aws-route_53_zone_and_records/issues/1

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "zone_name_servers" {
   value = aws_route53_zone.zone.name_servers
 }
 
+output "zone_arn" {
+  value = aws_route53_zone.zone.arn
+}
+
+output "zone_id" {
+  value = aws_route53_zone.zone_id
+}
